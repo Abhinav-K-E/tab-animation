b6b7e4f0af8640ebf2713d0d1090ae3cf3b4ed53
Abhinav <abhinavke091@gmail.com>
2024-05-14 10:45:57 +0530

    initial ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
+import { motion } from "framer-motion";
+
+const tabs = [
+  { id: 0, name: "Home" },
+  { id: 1, name: "About" },
+  { id: 2, name: "Lab" },
+  { id: 3, name: "Contact" },
+];
 export default function Home() {
-  return <></>;
+  return (
+    <div className="main w-full h-screen bg-zinc-900 flex items-center justify-center">
+      <div className="tabs p-2 rounded-lg bg-zinc-800 flex items-center gap-2">
+        {tabs.map((item) => (
+          <div key={item.id} className="tab px-8 py-4 bg-zinc-900 rounded-md text-white text-md">
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
